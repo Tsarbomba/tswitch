bbcfbbb30f43c6327d6c7e59b666dcb354ede82e
twitchswitch.contact@gmail.com <twitchswitch.contact@gmail.com@7b92194f-0698-44dd-6840-6fa0dca6aed7>
2015-02-17 00:43:30 +0000

    * Replaced SmartScroller since it didn't work well with "spammy" chat channels. * Changed green chat color since it was a bit too bright to read.

---
+++ TwitchSwitch/src/guiModule/ChatColors.java
@@ -0,0 +1,14 @@
+package guiModule;
+
+import java.awt.Color;
+
+public class ChatColors {
+    public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color DARK_BLUE = new Color(0, 0, 255);
+    public static final Color DARK_GREEN = new Color(41, 192, 41);
+    public static final Color RED = new Color(255, 0, 0);
+
+    private ChatColors() {
+
+    }
+}
